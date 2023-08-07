23f21523875111ad584d29b4b6878bb306dcf0ad
Pasha Ghomi <54184472+sirpasha@users.noreply.github.com>
2023-08-07 15:54:48 +0200

    Bash file created

---
+++ forwarder.sh
@@ -0,0 +1,11 @@
+read -p 'Enter Main Server\'s IP: ' IP
+echo "Your Main Server IP is: $IP"
+sudo apt  install iptables iptables-persistent
+sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -t nat -A POSTROUTING -p tcp --match multiport --dports  80,443,8443,2082,2083,2053,2095,8080 -j MASQUERADE 
+sudo iptables -t nat -A PREROUTING -p tcp --match multiport --dports  80,443,8443,2082,2083,2053,2095,8080 -j DNAT --to-destination $IP
+sudo iptables -t nat -A POSTROUTING -p udp -j MASQUERADE 
+sudo iptables -t nat -A PREROUTING -p udp -j DNAT --to-destination $IP
+
+sudo mkdir -p /etc/iptables/ 
+sudo iptables-save | sudo tee /etc/iptables/rules.v4
