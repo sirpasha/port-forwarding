14a42fb08d5e663b5224640adb3651b2f693f078
Pasha Ghomi <54184472+sirpasha@users.noreply.github.com>
2023-08-07 16:11:27 +0200

    Update forwarder.sh

--- forwarder.sh
+++ forwarder.sh
@@ -2,6 +2,8 @@ read -p 'Enter Main Server IP: ' IP
 echo "Your Main Server IP is: $IP"
 sudo apt  install iptables iptables-persistent
 sudo sysctl net.ipv4.ip_forward=1
+sudo iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 sudo iptables -t nat -A POSTROUTING -p tcp --match multiport --dports  80,443,8443,2082,2083,2053,2095,8080 -j MASQUERADE 
 sudo iptables -t nat -A PREROUTING -p tcp --match multiport --dports  80,443,8443,2082,2083,2053,2095,8080 -j DNAT --to-destination $IP
 sudo iptables -t nat -A POSTROUTING -p udp -j MASQUERADE 
